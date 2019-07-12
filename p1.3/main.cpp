868fffe90edce15854c1faf53c9f0b2f34a87ff8
Sergio Campos <morrodogg8@gmail.com>
2019-07-12 14:11:44 -0700

    finished project

--- p1.3/main.cpp
+++ p1.3/main.cpp
@@ -1,15 +1,29 @@
-//
-//  main.cpp
-//  p1.3
-//
-//  Created by Sergio campos on 5/19/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ ••• P1.3Write a program that prints your name in large letters, such as
+ 
+ *   *    **    ****    ****   *   *
+ *   *   *  *   *   *   *   *  *   *
+ *****  *    *  ****    ****    * *
+ *   *  ******  *   *   *   *    *
+ *   *  *    *  *    *  *    *   *
+ */
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "     ****       ******       *****     ****                                                   "<< endl;
+    cout << "     *          *           *****      *    *                                    "<< endl;
+    cout << "      *         *****                   *****                                         "<< endl;
+    cout << "       *        *           *   *            *                                 "<< endl;
+    cout << "      *****      ******     *   *            *                                         "<< endl;
+    cout << "                                        ******                                   "<< endl;
+    cout << "                                                                           "<< endl;
+    cout << "                                                                           "<< endl;
+    cout << "                                                                           "<< endl;
+    cout << "                                                                           "<< endl;
+    cout << "                                                                           "<< endl;
+
 }
